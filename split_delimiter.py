804065b37f91752de1f3804b523cf920a01ff512
theradman221 <theradman221@gmail.com>
2025-01-25 10:33:10 -0700

    Added Split Delimiter and associated tests, also made dummy entires for extract links and images, but haven't worked on yet

---
+++ split_delimiter.py
@@ -0,0 +1,30 @@
+import re
+from textnode import TextNode
+
+# Splits a old_nodes list / string and returns that as a list with any delimiters split out
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    return_list = []
+    for node in old_nodes:
+        # Make sure there is an opening and closing instance of the delimiter in the nodes text, or raise an exception if there isn't
+        text = node.text
+        if text.count(delimiter) % 2 != 0 and text.count(delimiter) > 0:
+            raise ValueError("There is no closing delimiter in this node")
+        splits = text.split(delimiter)
+        index = 0
+        for split in splits:
+            if index % 2 != 0:
+                # evens will be delimiter types, since you should always get an odd amount of splits from a successful split, and this is an index
+                return_list.append(TextNode(split, text_type))
+            else:
+                return_list.append(TextNode(split, node.text_type))
+            index += 1
+    return return_list
+
+def extract_markdown_images(text):
+    matches = set()
+    return matches
+
+def extract_markdown_links(text):
+    matches = set()
+    return matches
+            
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
+from split_delimiter import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 
 class TestTextNode(unittest.TestCase):
@@ -30,6 +31,70 @@ class TestTextNode(unittest.TestCase):
     def test_bad_type(self):
         with self.assertRaises(AttributeError):
             node = TextNode("Test Node", TextType.bald)
+
+    # Test the split_delimiter function
+    def test_delimiter(self):
+        # Test mismatched delimiter error
+        with self.assertRaises(ValueError):
+            node = TextNode("This will *be mismatched!", TextType.NORMAL)
+            failure = split_nodes_delimiter([node], "*", TextType.ITALIC)
+        expected_lists = [] # These should both be inserted to in the same order or you will be testing different test results
+        result_lists = []
+
+        test_node_none = TextNode("Oh what a lovely day for no splits", TextType.NORMAL)
+        expected_lists.append([TextNode("Oh what a lovely day for no splits", TextType.NORMAL)])
+        
+        test_node_bold = TextNode("Single **bold** word", TextType.NORMAL)
+        expected_lists.append([TextNode("Single ", TextType.NORMAL), TextNode("bold", TextType.BOLD), TextNode(" word", TextType.NORMAL)])
+        
+        test_node_bold_long = TextNode("Multiple **bolded** words in a sentance **is really fun**!", TextType.NORMAL)
+        expected_lists.append([TextNode("Multiple ", TextType.NORMAL), TextNode("bolded", TextType.BOLD), TextNode(" words in a sentance ", TextType.NORMAL), TextNode("is really fun", TextType.BOLD), TextNode("!", TextType.NORMAL)])
+        
+        test_node_italic = TextNode("this *should* be italics!", TextType.NORMAL)
+        expected_lists.append([TextNode("this ", TextType.NORMAL), TextNode("should", TextType.ITALIC), TextNode(" be italics!", TextType.NORMAL)])
+
+        test_node_single_bold = TextNode("**BOLD**", TextType.NORMAL)
+        expected_lists.append([TextNode("", TextType.NORMAL), TextNode("BOLD", TextType.BOLD), TextNode("", TextType.NORMAL)])
+
+        test_looping_types = TextNode("Going 4to4 .use. *multiple* **delimiters** in a 'loop' to make sure it @works@ properly", TextType.NORMAL)
+        expected_lists.append([
+            TextNode("Going ", TextType.NORMAL), TextNode("to", TextType.IMAGE), TextNode(" ", TextType.NORMAL), TextNode("use", TextType.CODE),
+            TextNode(" ", TextType.NORMAL), TextNode("multiple", TextType.ITALIC), TextNode(" ", TextType.NORMAL), TextNode("delimiters", TextType.BOLD),
+            TextNode(" in a ", TextType.NORMAL), TextNode("loop", TextType.CODE), TextNode(" to make sure it ", TextType.NORMAL), TextNode("works", TextType.LINK),
+            TextNode(" properly", TextType.NORMAL)
+        ])
+
+        # Process all test TextNodes into lists and compare the lists to the correct lists
+        none_list = split_nodes_delimiter([test_node_none], "**", TextType.BOLD) # There shouldn't be any splits in this list
+        result_lists.append(none_list)
+        bold_list = split_nodes_delimiter([test_node_bold], "**", TextType.BOLD) # This should result in a list like this, but with normal, bold, normal types [single, bold, word]
+        result_lists.append(bold_list)
+        bold_list_long = split_nodes_delimiter([test_node_bold_long], "**", TextType.BOLD)
+        result_lists.append(bold_list_long)
+        italic_list = split_nodes_delimiter([test_node_italic], "*", TextType.ITALIC)
+        result_lists.append(italic_list)
+        bold_single_list = split_nodes_delimiter([test_node_single_bold], "**", TextType.BOLD)
+        result_lists.append(bold_single_list)
+
+        # Test the looping to get multiple types of delimiters
+        looping_list = split_nodes_delimiter([test_looping_types], "**", TextType.BOLD)
+        looping_list = split_nodes_delimiter(looping_list, "4", TextType.IMAGE)
+        looping_list = split_nodes_delimiter(looping_list, "'", TextType.CODE)
+        looping_list = split_nodes_delimiter(looping_list, "@", TextType.LINK)
+        looping_list = split_nodes_delimiter(looping_list, ".", TextType.CODE)
+        looping_list = split_nodes_delimiter(looping_list, "*", TextType.ITALIC)
+        result_lists.append(looping_list)
+        index = 0
+        while index < len(expected_lists) and index < len(result_lists):
+            inner_index = 0
+            while inner_index < len(expected_lists[index]) and inner_index < len(result_lists[index]):
+                self.assertEqual(expected_lists[index][inner_index], result_lists[index][inner_index])
+                inner_index += 1
+            index += 1
+
+    def test_markdown_extractors(self):
+        text = ""
+
 
 
 if __name__ == "__main__":
